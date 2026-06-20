57b86027ed67b78a7a2cd928d82ba4980cd7a01a
david Henriquez <152029007+jedavid99@users.noreply.github.com>
2026-06-20 20:14:52 -0300

    add

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://overlix-demo-backend-production.up.railway.app',
+  baseURL: 'https://overlix-demo-backend-production.up.railway.app/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
