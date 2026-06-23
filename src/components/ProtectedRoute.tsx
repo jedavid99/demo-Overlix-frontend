4816768cc85c2c2b6572cccfc4d16f3ed8aa7583
david Henriquez <152029007+jedavid99@users.noreply.github.com>
2026-06-23 17:34:23 -0300

    add

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -18,7 +18,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   }
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return <>{children}</>;
