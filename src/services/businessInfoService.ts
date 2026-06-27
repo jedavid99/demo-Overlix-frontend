a9a1dd32830c069b639c64e727b756e3d0f157f9
david Henriquez <152029007+jedavid99@users.noreply.github.com>
2026-06-27 18:11:55 -0300

    add

--- src/services/businessInfoService.ts
+++ src/services/businessInfoService.ts
@@ -4,16 +4,16 @@ import { BusinessInfo, BusinessInfoUpdate } from '@/types/businessInfo.types';
 export const businessInfoService = {
   // Obtener información de la empresa
   get: (): Promise<BusinessInfo> => {
-    return api.get('/api/business-info').then(res => res.data);
+    return api.get('/business-info').then(res => res.data);
   },
 
   // Actualizar información de la empresa
   update: (data: BusinessInfoUpdate): Promise<BusinessInfo> => {
-    return api.put('/api/business-info', data).then(res => res.data);
+    return api.put('/business-info', data).then(res => res.data);
   },
 
   // Actualizar logo
   updateLogo: (logoUrl: string): Promise<{ logo_url: string }> => {
-    return api.put('/api/business-info/logo', { logo_url: logoUrl }).then(res => res.data);
+    return api.put('/business-info/logo', { logo_url: logoUrl }).then(res => res.data);
   }
 };
